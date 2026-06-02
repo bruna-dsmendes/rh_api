071a3f61c028c5e88286bce4f9316bb6983be1dd
Bruna Mendes <brunadsantos.mendes@gmail.com>
2026-06-02 11:56:59 -0300

    Inicialização do projeto

---
+++ src/test/java/com/generation/cadastrorh/RhApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.cadastrorh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
